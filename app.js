cb5550e93c5864f7e732e4e7fc8db67fa38aa039
Yaroslav <eragon2193@gmail.com>
2023-02-18 10:50:17 +0100

    working version

---
+++ app.js
@@ -0,0 +1,67 @@
+addEventListener('DOMContentLoaded', () => {
+  const myLibrary = [{ author: 'Rowling', title: 'Harry Potter', pages: 255 },
+    { author: 'Rowling', title: 'Harry Potter', pages: 255 },
+    { author: 'Rowling', title: 'Harry Potter', pages: 255 }]
+  const grid = document.getElementById('grid-book')
+  function Book (author, title, pages, checked) {
+    this.author = author
+    this.title = title
+    this.pages = pages
+    this.checked = checked
+  }
+  const clearForm = () => {
+  }
+  const createDivs = () => {
+    myLibrary.forEach((a) => {
+      const p1 = document.createElement('p')
+      p1.innerText = a.title
+      const p2 = document.createElement('p')
+      p2.innerText = a.author
+      const p3 = document.createElement('p')
+      p3.innerText = a.pages
+      const checkBoxDiv = document.createElement('div')
+      checkBoxDiv.classList.add('cardCheckBox')
+      const p4 = document.createElement('p')
+      p4.innerText = 'Read'
+      const checkBox = document.createElement('input')
+      checkBox.setAttribute('type', 'checkbox')
+      checkBox.checked = a.checked
+      checkBoxDiv.append(p4, checkBox)
+      const div = document.createElement('div')
+      div.classList.add('card')
+      div.append(p1, p2, p3, checkBoxDiv)
+      grid.append(div)
+    })
+  }
+  const addButton = document.getElementById('add-button')
+  addButton.addEventListener('click', () => {
+    const form = document.getElementById('form')
+    form.reset()
+    form.classList.toggle('hidden')
+  })
+
+  const addBook = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const checked = document.getElementById('checkbox').checked
+    console.log(checked)
+    const book = new Book(author, title, pages, checked)
+    myLibrary.push(book)
+  }
+
+  const submitButton = document.getElementById('submit')
+  submitButton.addEventListener('click', function (event) {
+    event.preventDefault()
+    addBook()
+    clearForm()
+    while (grid.hasChildNodes()) {
+      grid.removeChild(grid.lastChild)
+    }
+    createDivs()
+    const form = document.getElementById('form')
+    form.classList.toggle('hidden')
+  })
+
+  createDivs()
+})
